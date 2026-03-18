ea02ba58d4a212ec7b843ca39cc13f6db03afb8c
Immanuel Rajkumar <immanuelrajkumar@Immanuels-Mac-mini.local>
2026-03-18 10:15:46 +0530

    Fix: bash while loop with proper error handling

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 node /app/init-volume.js 2>/dev/null || true
-while true; do
-  node /app/index.js
-  echo "Restarting in 5s..."
-  sleep 5
+while :; do
+  node /app/index.js || true
+  sleep 2
 done
